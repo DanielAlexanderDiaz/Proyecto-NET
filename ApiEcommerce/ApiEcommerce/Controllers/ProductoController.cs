4b5235cd58e2e94c70aea589288ba69cb3281446
Daniel Alexander <danieldiazalexander@gmail.com>
2026-07-27 08:38:08 -0400

    Endpoint: Crear producto asociado a categoría

--- ApiEcommerce/ApiEcommerce/Controllers/ProductoController.cs
+++ ApiEcommerce/ApiEcommerce/Controllers/ProductoController.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Models;
 using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository.IRepository;
 using AutoMapper;
@@ -11,11 +12,13 @@ namespace ApiEcommerce.Controllers
     public class ProductoController : ControllerBase
     {
         private readonly IProductoRepository _productoRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
         private readonly IMapper _mapper;
 
-        public ProductoController(IProductoRepository productoRepository, IMapper mapper)
+        public ProductoController(IProductoRepository productoRepository, ICategoriaRepository categoriaRepository, IMapper mapper)
         {
             _productoRepository = productoRepository;
+            _categoriaRepository = categoriaRepository;
             _mapper = mapper;
         }
 
@@ -43,6 +46,37 @@ namespace ApiEcommerce.Controllers
             }
             var productoDto = _mapper.Map<ProductoDTO>(producto);
             return Ok(productoDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CrearProducto([FromBody] CrearProductoDTO crearProductoDTO)
+        {
+            if (crearProductoDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (_productoRepository.ProductoExiste(crearProductoDTO.Nombre))
+            {
+                ModelState.AddModelError("CustomError", "Producto ya existe");
+                return BadRequest(ModelState);
+            }
+            if (!_categoriaRepository.CategoriaExiste(crearProductoDTO.CategoriaId))
+            {
+                ModelState.AddModelError("CustomError", "categoria no existe");
+                return BadRequest(ModelState);
+            }
+            var producto = _mapper.Map<Producto>(crearProductoDTO);
+            if (!_productoRepository.CrearProducto(producto))
+            {
+                ModelState.AddModelError("CustomError", $"Algo salió mal guardando el registro {producto.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetProducto", new { id = producto.ProductoId }, producto);
         }
     }
 }
