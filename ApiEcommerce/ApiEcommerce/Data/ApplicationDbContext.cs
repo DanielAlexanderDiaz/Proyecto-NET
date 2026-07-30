8e755fbc2b9ab002e316723dd60393985932dba5
Daniel Alexander <danieldiazalexander@gmail.com>
2026-07-29 23:30:15 -0400

    Modelo usuario

--- ApiEcommerce/ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/ApiEcommerce/Data/ApplicationDbContext.cs
@@ -9,4 +9,5 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Producto> Productos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
 }
---
+++ ApiEcommerce/ApiEcommerce/Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApiEcommerce.Models;
+
+public class Usuario
+{
+    public int Id {get; set; }
+    public string? Nombre {get; set; }
+    public string? NombreUsuario {get; set; }
+    public string? Password {get; set; }
+    public string? Role {get; set; }
+}
