73888abf19a1e989623fa18f73a63889fcf2d2dc
Daniel Alexander <danieldiazalexander@gmail.com>
2026-07-31 00:21:41 -0400

    Desarrollo método Login

--- ApiEcommerce/ApiEcommerce/Repository/UsuarioRepository.cs
+++ ApiEcommerce/ApiEcommerce/Repository/UsuarioRepository.cs
@@ -2,15 +2,18 @@ using System;
 using ApiEcommerce.Models;
 using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiEcommerce.Repository;
 
 public class UsuarioRepository : IUsuarioRepository
 {
-    private readonly ApplicationDbContext _db;
-    public UsuarioRepository(ApplicationDbContext db)
+    public readonly ApplicationDbContext _db;
+    private string? secretKey;
+    public UsuarioRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
     }
 
     public bool EsUnicoElNombre(string nombre)
@@ -28,9 +31,39 @@ public class UsuarioRepository : IUsuarioRepository
         return _db.Usuarios.OrderBy(u => u.NombreUsuario).ToList();
     }
 
-    public Task<UsuarioLoginResponseDTO> Login(UsuarioLoginDTO usuarioLoginDTO)
+    public async Task<UsuarioLoginResponseDTO> Login(UsuarioLoginDTO usuarioLoginDTO)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(usuarioLoginDTO.Name))
+        {
+            return new UsuarioLoginResponseDTO()
+            {
+                Token= "",
+                Usuario = null,
+                Mensaje = "El nombre es requerido"
+            };
+        }
+
+        var usuario = await _db.Usuarios.FirstOrDefaultAsync<Usuario>(u => u.NombreUsuario.ToLower().Trim() == usuarioLoginDTO.Name.ToLower().Trim());
+        if (usuario == null)
+        {
+            return new UsuarioLoginResponseDTO()
+            {
+                Token= "",
+                Usuario = null,
+                Mensaje = "El nombre no encontrado"
+            };
+        }
+        if (!BCrypt.Net.BCrypt.Verify(usuarioLoginDTO.Password, usuario.Password))
+        {
+            return new UsuarioLoginResponseDTO()
+            {
+                Token= "",
+                Usuario = null,
+                Mensaje = "Credenciales incorrectas"
+            };
+        }
+
+        return null;
     }
 
     public async Task<Usuario> Register(CrearUsuarioDTO crearUsuarioDTO)
